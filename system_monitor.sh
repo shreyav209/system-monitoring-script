6ea2e9770138df976eb46683e7d9dbfe9ffbdfe9
shreya209 <shreya.vethekar09@gmail.com>
2026-08-25 20:30:54 +0530

    Add Disk usage

--- system_monitor.sh
+++ system_monitor.sh
@@ -37,4 +37,28 @@ echo "===================================================" >> "$LOG_FILE"
 echo
 echo "$(date '+%Y-%m-%d %H:%M:%S') - System Monitoring Started!!!" >> "$LOG_FILE"
 
+#------------------------------------
+#Disk  Monitoring
+#------------------------------------
+
+
+echo "-----------DISK USAGE--------------"
+
+disk_usage=$(df / | awk 'NR==2 {print $5}' | tr -d '%')
+
+echo "Root filesystem usage : ${disk_usage}%"
+
+#Throw alert message
+if [ "$disk_usage" -ge "$DISK_THRESHOLD" ];
+then
+        echo "WARNING: Disk usage is high: ${disk_usage}%"
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - WARNING: Disk usage is ${disk_usage}%" >> "$LOG_FILE"
+else
+        echo "Disk usage is normal: ${disk_usage}%"
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - Disk usage is normal: ${disk_usage}%" >> "$LOG_FILE"
+fi
+
+echo
+echo
+echo
 
